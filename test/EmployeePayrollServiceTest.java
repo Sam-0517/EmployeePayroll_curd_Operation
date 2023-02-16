5f4183992e0438f4f3eba73d5a26f26365534b92
Sam-0517 <samadhankorde2000@gmail.com>
2023-02-16 21:02:38 +0530

    Added: Employee payroll using Curd operation

---
+++ test/EmployeePayrollServiceTest.java
@@ -0,0 +1,16 @@
+package com.sql.jdbc;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrived_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+        Assert.assertEquals(3, employeePayrollData.size());
+    }
+
+}
